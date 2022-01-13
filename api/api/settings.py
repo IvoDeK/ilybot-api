587ab2c0b58cfbaca4016274c040c9a9efacf92e
IvoDeK <ivodoetyt@gmail.com>
2022-01-13 11:01:40 +0100

    Added external database connection with environment variables

--- api/api/settings.py
+++ api/api/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+# Make it so I can add environment variables.
+import environ
+
+env = environ.Env()
+
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,8 +82,12 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DBNAME'),
+        'USER': env('DBUSER'),
+        'PASSWORD': env('DBPASS'),
+        'HOST': env('DBIP'),
+        'PORT': '3306',
     }
 }
 
